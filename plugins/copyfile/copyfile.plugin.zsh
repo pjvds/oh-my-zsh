c33672f147e90ac0fc575e4ac5985feb557108b8
Pieter Joost van de Sande <pj@born2code.net>
2015-08-27 11:39:18 +0200

    add pbcopy check to copyfile plugin

--- plugins/copyfile/copyfile.plugin.zsh
+++ plugins/copyfile/copyfile.plugin.zsh
@@ -1,5 +1,13 @@
 function copyfile {
   [[ "$#" != 1 ]] && return 1
   local file_to_copy=$1
-  cat $file_to_copy | pbcopy
+
+  if type pbcopy &> /dev/null; then
+    cat $file_to_copy | pbcopy
+    pwd | tr -d "\r\n" | pbcopy
+  else
+    echo 'pbcopy not found'
+    echo 'Please check if pbcopy is correctly installed.'
+    return 1
+  fi
 }
