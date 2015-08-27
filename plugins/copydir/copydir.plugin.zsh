ecce50a256da8b9855c07eeb698f3cd3fd169028
Pieter Joost van de Sande <pj@born2code.net>
2015-08-27 11:37:22 +0200

    add pbcopy check to copydir plugin

--- plugins/copydir/copydir.plugin.zsh
+++ plugins/copydir/copydir.plugin.zsh
@@ -1,3 +1,9 @@
 function copydir {
-  pwd | tr -d "\r\n" | pbcopy
+  if type pbcopy &> /dev/null; then
+    pwd | tr -d "\r\n" | pbcopy
+  else
+    echo 'pbcopy not found'
+    echo 'Please check if pbcopy is correctly installed.'
+    return 1
+  fi
 }
