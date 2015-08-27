7b5bb7d46432233c9a99974a76d1c40a2e2d252d
Pieter Joost van de Sande <pj@born2code.net>
2015-08-27 12:07:10 +0200

    add xsel fallback to copydir

--- plugins/copydir/copydir.plugin.zsh
+++ plugins/copydir/copydir.plugin.zsh
@@ -1,9 +1,11 @@
 function copydir {
   if type pbcopy &> /dev/null; then
     pwd | tr -d "\r\n" | pbcopy
+  elif type xsel &> /dev/null; then
+    pwd | tr -d "\r\n" | xsel --clipboard --input
   else
-    echo 'pbcopy not found'
-    echo 'Please check if pbcopy is correctly installed.'
+    echo 'No clipboard util found!'
+    echo 'Please install pbcopy when you are on Mac OSX or xsel on Linux.'
     return 1
   fi
 }
