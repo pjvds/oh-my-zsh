41298e366e4d087b4176edbb098755b7cd87e7b8
Pieter Joost van de Sande <pj@born2code.net>
2015-08-27 12:09:54 +0200

    add xclip fallback to copydir

--- plugins/copydir/copydir.plugin.zsh
+++ plugins/copydir/copydir.plugin.zsh
@@ -3,9 +3,14 @@ function copydir {
     pwd | tr -d "\r\n" | pbcopy
   elif type xsel &> /dev/null; then
     pwd | tr -d "\r\n" | xsel --clipboard --input
+  elif type xclip &> /dev/null; then
+    pwd | tr -d "\r\n" | xclip --selection clipboard
   else
     echo 'No clipboard util found!'
-    echo 'Please install pbcopy when you are on Mac OSX or xsel on Linux.'
+    echo 'Please install one of the following utils:'
+    echo '\tpbcopy'
+    echo '\txsel'
+    echo '\txclip'
     return 1
   fi
 }
